703fc207247e19cf6115dad5565c8a91b4fafab0
Kurják Richárd <kurjak.richard@gmail.com>
2024-02-14 02:16:36 +0100

    File picker v1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,12 +10,11 @@ void main() {
 class Flutibre extends StatelessWidget {
   const Flutibre({super.key});
 
-   @override
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -31,12 +33,39 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  FilePickerResult? result;
+  String? _fileName;
+  PlatformFile? pickedfile;
+  bool isLoading = false;
+  File? fileToDisplay;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  void pickFile() async {
+    try {
+      setState(() {
+        isLoading = true;
+      });
+      result = await FilePicker.platform.pickFiles(
+          type: FileType.any,
+          allowMultiple: false,
+          allowedExtensions: ['pdf', 'odt']);
+
+      if (result != null) {
+        _fileName = result!.files.first.name;
+        pickedfile = result!.files.first;
+        fileToDisplay = File(pickedfile!.path.toString());
+      } else {
+        setState(() {
+          isLoading = false;
+        });
+        return;
+      }
+
+      setState(() {
+        isLoading = false;
+      });
+    } catch (e) {
+      print(e);
+    }
   }
 
   @override
@@ -46,25 +75,23 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: isLoading
+                ? const CircularProgressIndicator()
+                : pickedfile != null
+                    ? Text(_fileName.toString())
+                    : const Text(''),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: pickFile,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), 
-          );
+      ),
+    );
   }
 }
