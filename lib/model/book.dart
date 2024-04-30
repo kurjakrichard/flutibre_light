3970f8f770622d8aa5b6f0fa54b4c2a8397e48c5
Kurják Richárd <kurjak.richard@gmail.com>
2024-04-30 23:48:03 +0200

    Backup

--- lib/model/book.dart
+++ lib/model/book.dart
@@ -1,14 +1,13 @@
+// ignore_for_file: must_be_immutable, non_constant_identifier_names
+
 import 'package:equatable/equatable.dart';
 
-// ignore: must_be_immutable
 class Book extends Equatable {
   int? id;
   final String title;
-  // ignore: non_constant_identifier_names
   final String author_sort;
   final String path;
 
-  // ignore: non_constant_identifier_names
   Book({this.id, this.title = '', this.author_sort = '', this.path = ''});
 
   //Convert a Map object to a model object
